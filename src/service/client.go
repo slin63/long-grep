96f3c61da314f24c0cda4e8c5eae5ab2c51edd78
knoppers knoppers <knoppers@knoppers.com>
2020-01-22 17:01:03 -0600

    client now asynchronously grabs and compiles greps

--- src/service/client.go
+++ src/service/client.go
@@ -14,6 +14,8 @@ import (
 
 // Client for querying logs
 func Client(expressionPtr *string) {
+	logs := make(chan string)
+	results := make(chan string)
 	var wg sync.WaitGroup
 
 	addresses, err := config.Addresses()
@@ -25,22 +27,35 @@ func Client(expressionPtr *string) {
 	for _, address := range addresses {
 		log.Println(address)
 		wg.Add(1)
-		go grepLogs(address, expressionPtr, &wg)
+		go grepLogs(address, expressionPtr, &wg, logs)
 	}
+	go compileLogs(logs, results)
 
 	wg.Wait()
+	close(logs)
+	fmt.Println(<-results)
 }
 
-func grepLogs(address string, expressionPtr *string, wg *sync.WaitGroup) string {
+func compileLogs(logs, results chan string) {
+	var compiled, log string
+	ok := true
+	for ok {
+		log, ok = <-logs
+		compiled += log
+	}
+	results <- compiled
+}
+
+func grepLogs(address string, expressionPtr *string, wg *sync.WaitGroup, logs chan string) {
 	defer wg.Done()
 	log.Println("Dispatching RPC for", address)
-	var logs string
+	var reply string
 	client, err := rpc.DialHTTP("tcp", address)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client.Call("Logly.GrepLogs", *expressionPtr, &logs)
-	log.Println("done:", address, len(logs))
-	return logs
+	client.Call("Logly.GrepLogs", *expressionPtr, &reply)
+	log.Println("done:", address, len(reply))
+	logs <- reply
 }
